192deb65ea8bbb4c2b1524a5453b953d401620ba
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-02-27 16:30:40 +0600

    Final Commit

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-let playArea = {};
-playArea.inPlay = false;
-const message = document.querySelector(".message");
-const button = document.querySelector("button");
-const gameArea = document.querySelector(".gameArea");
+let playArea = {}
+playArea.inPlay = false
+const message = document.querySelector('.message')
+const button = document.querySelector('button')
+const gameArea = document.querySelector('.gameArea')
 
-messager("Click start to Play");
+messager('Click start to Play')
 
-button.addEventListener("click", function () {
-    if (!playArea.inPlay) {
-        playArea.inPlay = true;
-        button.style.display = "none";
-        messager("Click the Enemy as quickly as you see them");
-        playArea.timer = setTimeout(myBox, rand(3000));
-    }
+button.addEventListener('click', function () {
+  if (!playArea.inPlay) {
+    playArea.inPlay = true
+    button.style.display = 'none'
+    messager('Click the Enemy as quickly as you see them')
+    playArea.timer = setTimeout(myBox, rand(3000))
+  }
 })
 
 function myBox() {
-    let el = document.createElement("div");
-    el.style.backgroundImage = "url(11.png)";
-    el.style.backgroundRepeat = "no-repeat";
-    el.style.backgroundPosition = "center";
-    el.style.backgroundSize = "cover";
-    el.style.width = 100 + "px";
-    el.style.height = 100 + "px";
-    el.style.position = "relative";
-    el.style.top = rand(500) + "px";
-    el.style.left = rand(80) + "vw";
-    el.addEventListener("click", hit);
-    el.start = new Date().getTime();
-    gameArea.appendChild(el);
+  let el = document.createElement('div')
+  el.style.backgroundImage = 'url(images/11.png)'
+  el.style.backgroundRepeat = 'no-repeat'
+  el.style.backgroundPosition = 'center'
+  el.style.backgroundSize = 'cover'
+  el.style.width = 100 + 'px'
+  el.style.height = 100 + 'px'
+  el.style.position = 'relative'
+  el.style.top = rand(500) + 'px'
+  el.style.left = rand(80) + 'vw'
+  el.addEventListener('click', hit)
+  el.start = new Date().getTime()
+  gameArea.appendChild(el)
 }
 
 function hit(e) {
-    let end = new Date().getTime();
-    let start = e.target.start;
-    let duration = (end - start) / 1000;
-    messager("It took " + duration + " seconds to HeadShot!");
-    clearTimeout(playArea.timer);
-    gameArea.children[0].remove();
-    playArea.timer = setTimeout(myBox, rand(3000));
+  let end = new Date().getTime()
+  let start = e.target.start
+  let duration = (end - start) / 1000
+  messager('It took ' + duration + ' seconds to HeadShot!')
+  clearTimeout(playArea.timer)
+  gameArea.children[0].remove()
+  playArea.timer = setTimeout(myBox, rand(3000))
 }
 
 /* function getColor() {
@@ -50,10 +50,10 @@ function hit(e) {
 } */
 
 function rand(num) {
-    let tempVal = Math.floor(Math.random() * num);
-    return tempVal;
+  let tempVal = Math.floor(Math.random() * num)
+  return tempVal
 }
 
 function messager(mes) {
-    message.innerHTML = mes;
+  message.innerHTML = mes
 }
